4bfbdfb28f373b8db168ce8ef510d507080d9861
Timo Uelen <timo.uelen@coolblue.nl>
2019-09-27 10:08:29 +0200

    Refactor more stuff into functions!

--- create_features_python.py
+++ create_features_python.py
@@ -7,11 +7,14 @@ import dask.array as da
 
 
 NUMBER_OF_TOP_POPULAR_PRODUCTS = 6000
+MOVING_WINDOW_SIZE = 4
+REMEMBER_WINDOW_SIZE = 4
+MOVING_WINDOW_SKIP_SIZE = 1
 
 
 def load_data():
     return dd.read_csv(
-        "./data/input_small.csv", encoding="utf-8", dtype=str, na_values="null"
+        "./data/input.csv", encoding="utf-8", dtype=str, na_values="null"
     )
 
 
@@ -58,34 +61,18 @@ def encode_sessions(product_ids_masking, raw_sessions):
     return encoded_sessions
 
 
-def main():
-    raw_data = load_data()
-    print(raw_data.head())
-    raw_sessions = extract_raw_sessions(raw_data)
+def get_valid_sessions(encoded_sessions):
+    # Sequence of 1 doesn't have a target ;)
+    return [x for x in encoded_sessions if len(x) > 1]
 
-    popular_products = get_popular_products(raw_sessions)
-    # popular_products_encoded = list(range(NUMBER_OF_TOP_POPULAR_PRODUCTS))
 
-    product_ids_masking = create_product_ids_masking(popular_products)
-    product_ids_masking_reversed = create_product_ids_masking_reversed(popular_products)
-
-    # encoding
-    encoded_sessions = encode_sessions(product_ids_masking, raw_sessions)
-
-    # Valid sequences are those that at least have 2 products belonging to the top N
-    # popular products
-    encoded_sessions = [x for x in encoded_sessions if len(x) > 1]
-
+def extract_sequences_vs_targets(encoded_sessions):
     # Extract sequences and targets
     sequences_vs_targets = []
 
     # Only keep the x latest items that can fit in the REMEMBER_WINDOW_SIZE
-    MOVING_WINDOW_SIZE = 4
-    REMEMBER_WINDOW_SIZE = 4
-    MOVING_WINDOW_SKIP_SIZE = 1
-
     number_of_latest_items_to_fetch = MOVING_WINDOW_SIZE + (
-        (REMEMBER_WINDOW_SIZE - 1) * MOVING_WINDOW_SKIP_SIZE
+            (REMEMBER_WINDOW_SIZE - 1) * MOVING_WINDOW_SKIP_SIZE
     )
     for session in encoded_sessions:
         targets = session[1:]
@@ -94,25 +81,28 @@ def main():
             base = session[: index + 1][-number_of_latest_items_to_fetch:]
             len_base_minus_window_size = len(base) - REMEMBER_WINDOW_SIZE
             number_of_active_remember_windows = (
-                math.ceil(len_base_minus_window_size / MOVING_WINDOW_SKIP_SIZE) + 1
+                    math.ceil(len_base_minus_window_size / MOVING_WINDOW_SKIP_SIZE) + 1
             )
             if number_of_active_remember_windows < 1:
                 number_of_active_remember_windows = 1
             sequences = [
-                base[i : i + MOVING_WINDOW_SIZE]
+                base[i: i + MOVING_WINDOW_SIZE]
                 for i in range(number_of_active_remember_windows)
             ]
             sequences = ([[]] * (REMEMBER_WINDOW_SIZE - len(sequences))) + sequences
             intervals.append(sequences)
         sequences_vs_targets.append([intervals, targets])
 
-    print(sequences_vs_targets)
+    # print(sequences_vs_targets)
+    return sequences_vs_targets
 
+
+def encode_binary(sequences_vs_targets):
     # binary encoding
     OUTPUT_PROCESSING_CHUNK_SIZE = 10000
     number_of_sequences = len(sequences_vs_targets)
     chunks = [
-        sequences_vs_targets[x : x + OUTPUT_PROCESSING_CHUNK_SIZE]
+        sequences_vs_targets[x: x + OUTPUT_PROCESSING_CHUNK_SIZE]
         for x in range(0, number_of_sequences, OUTPUT_PROCESSING_CHUNK_SIZE)
     ]
 
@@ -141,8 +131,29 @@ def main():
 
         dataset_X = da.from_array(np.vstack(combined_X), chunks=-1)
         dataset_y = da.from_array(np.vstack(combined_y), chunks=-1)
-        print("end")
+    return dataset_X, dataset_y
 
+
+def main():
+    raw_data = load_data()
+    print(raw_data.head())
+    raw_sessions = extract_raw_sessions(raw_data)
+
+    popular_products = get_popular_products(raw_sessions)
+    popular_products_encoded = list(range(NUMBER_OF_TOP_POPULAR_PRODUCTS))
+
+    product_ids_masking = create_product_ids_masking(popular_products)
+    product_ids_masking_reversed = create_product_ids_masking_reversed(popular_products)
+
+    # encoding
+    encoded_sessions = encode_sessions(product_ids_masking, raw_sessions)
+    encoded_sessions = get_valid_sessions(encoded_sessions)
+
+    sequences_vs_targets = extract_sequences_vs_targets(encoded_sessions)
+
+    X, y = encode_binary(sequences_vs_targets)
+    print(f"length of X and y: {len(X)}, {len(y)}")
+    print("end")
 
 if __name__ == "__main__":
     main()
