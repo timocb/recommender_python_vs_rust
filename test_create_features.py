0ee5e0c1299ce9fef4e5fdc9190b881ba1a812c9
Timo Uelen <timo.uelen@coolblue.nl>
2019-09-27 13:54:20 +0200

    Add test_input_csv

--- test_create_features.py
+++ test_create_features.py
@@ -9,17 +9,12 @@ import create_features_python
 
 def mock_load_data():
     return pd.read_csv(
-        "./data/input.csv", encoding="utf-8", dtype=str, na_values="null", nrows=1000
+        "./data/test_input_data.csv", encoding="utf-8", dtype=str, na_values="null"
     )
 
 
 @mock.patch("create_features_python.load_data", mock_load_data)
 def test_main():
-    from pathlib import Path
-    print(f"Current working dir: {Path.cwd()}\n")
-    print([x for x in Path('.').iterdir() if x.is_dir()])
-    print()
-
     with open("./data/test_X_y.pickle", "rb") as f:
         expected_X, expected_y = pickle.load(f)
 
